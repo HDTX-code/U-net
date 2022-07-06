50128688439b1ff0d20799a05bc276e2b878e9a5
HDTX-code <1252900424@qq.com>
2022-07-06 13:03:47 +0800

    2.5D + Focal loss(带权重)

--- train_sh/train.sh
+++ train_sh/train.sh
@@ -3,13 +3,16 @@
 conda activate homefun
 # cd /devdata/home/homefun/U-net/
 cd /Home/atr2/homefun/zhf/U-net
-nohup python train.py --GPU 2 \
+nohup python train.py --GPU 1 \
                       --Freeze_batch_size 148 \
                       --UnFreeze_batch_size 28 \
                       --train weights/all/train.txt \
                       --val weights/all/val.txt \
                       --cls_weights 0.3 0.7 \
+                      --resume weights/loss_20220705182252/best_model_res50.pth \
+                      --Freeze_Epoch 0 \
+                      --UnFreeze_Epoch 220 \
                       --amp \
                       --save_best \
                       --pretrain_backbone \
-                      --bilinear > weights/logres50_1.txt 2>&1 &
+                      --bilinear > weights/logres50_2.txt 2>&1 &
--- train_sh/train_mutil_GPU.sh
+++ train_sh/train_mutil_GPU.sh
@@ -4,12 +4,16 @@ conda activate homefun
 # cd /devdata/home/homefun/U-net/
 cd /Home/atr2/homefun/zhf/U-net
 CUDA_VISIBLE_DEVICES=0,2 nohup \
-python -m torch.distributed.launch --nproc_per_node=2 train_mutil_GPU.py  --Freeze_batch_size 148 \
-                                                                          --UnFreeze_batch_size 28 \
-                                                                          --train weights/all/train.txt \
-                                                                          --val weights/all/val.txt \
-                                                                          --cls_weights 0.3 0.7 \
-                                                                          --amp \
-                                                                          --save_best \
-                                                                          --pretrain_backbone \
-                                                                          --bilinear >weights/logres50_1.txt 2>&1 &
+python -m torch.distributed.launch --nproc_per_node=2 \
+train_mutil_GPU.py  --Freeze_batch_size 148 \
+                    --UnFreeze_batch_size 28 \
+                    --train weights/all/train.txt \
+                    --val weights/all/val.txt \
+                    --cls_weights 0.3 0.7 \
+                    --resume weights/loss_20220705182252/best_model_res50.pth \
+                    --Freeze_Epoch 0 \
+                    --UnFreeze_Epoch 220 \
+                    --amp \
+                    --save_best \
+                    --pretrain_backbone \
+                    --bilinear >weights/logres50_2.txt 2>&1 &
