8a1e3eacb7d493f3b34a4625b04862b0ef077a2c
HDTX-code <1252900424@qq.com>
2022-07-09 22:19:09 +0800

    增加lv loss与iou loss

--- train_sh/train.sh
+++ train_sh/train.sh
@@ -5,25 +5,26 @@ cd /devdata/home/homefun/U-net/
 #cd /Home/atr2/homefun/zhf/U-net
 nohup python train.py --GPU 0 \
                       --Freeze_batch_size 512 \
-                      --UnFreeze_batch_size 128 \
+                      --UnFreeze_batch_size 108 \
                       --train weights/all/train.txt \
                       --val weights/all/val.txt \
                       --cls_weights 0.5 0.5 \
-                      --resume weights/loss_20220708114311/best_model_res50.pth \
+                      --pretrained weights/loss_20220708114311/best_model_res50.pth \
                       --size 320 \
                       --Freeze_Epoch 0 \
-                      --UnFreeze_Epoch 320 \
+                      --UnFreeze_Epoch 100 \
                       --Init_lr_Freeze 1e-4 \
-                      --Init_lr_UnFreeze 1e-3 \
+                      --Init_lr_UnFreeze 1e-4 \
                       --Min_lr_Freeze 1e-5 \
-                      --Min_lr_UnFreeze 1e-5 \
+                      --Min_lr_UnFreeze 1e-6 \
                       --amp \
                       --save_best \
                       --bilinear \
                       --loss_ce \
                       --loss_focal \
                       --loss_lv > weights/logres50_4.txt 2>&1 &
-#                      --Auto_Freeze \
-#                      --Auto_UnFreeze
+#                     --resume weights/loss_20220708114311/best_model_res50.pth \
+#                     --Auto_Freeze \
+#                     --Auto_UnFreeze
 #                     --loss_iou \
 #                     --pretrain_backbone \
